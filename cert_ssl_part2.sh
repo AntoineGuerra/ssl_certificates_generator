fce290954ca1ae6000189d0c9965ff11d4d65d94
antoine <antoine.guerra@epitech.eu>
2019-01-19 23:52:12 +0100

    add conf generator

--- cert_ssl_part2.sh
+++ cert_ssl_part2.sh
@@ -8,10 +8,41 @@ color_error='\033[0;31m'
 path_to_certificates_directory=''
 name_of_middle_certificate=''
 certificates=()
+
+
+addSlash() {
+    if [[ !(${path_to_certificates_directory} =~ (.*)\/$) ]]
+    then
+        path_to_certificates_directory="${path_to_certificates_directory}/"
+    fi
+}
+
+# $1 = file data checked
+checkCertFilesData() {
+    file=$1
+#    if [[ ${file} = './config.conf' ]]
+#    then
+#        datas=('email')
+#    else
+        datas=('company_name' 'hostname_company')
+#    fi
+    for data in ${datas[@]}
+    do
+        value=$(eval echo "\$$data")
+        if [[ $value = '' ]]
+        then
+            echo -e "There is an${color_error} ERROR ${color_default} in ${color_info}${file}"
+            echo -e "${color_error}${data//_/ } is REQUIRED !"
+            exit
+        fi
+    done
+}
+
+
 # $1 = path_to_certificates_directory
 createVal() {
     varName=$1
-    echo -ne "${color_default}Type ${color_success}${varName//_/ }${color_default} and press ${color_success}ENTER :\n"
+    echo -ne "${color_default}Type ${color_success}${varName//_/ }${color_default} and press ${color_success}ENTER :${color_default}\n"
     if [[ ${path_to_certificates_directory} != '' && ${varName} = 'path_to_certificates_directory' ]]
     then
         read -e -r -p "$ " -i ${path_to_certificates_directory} result
@@ -40,7 +71,11 @@ renameCertificates() {
         if [[ $file =~ (.*)\/([^\/]*)\.crt$ ]];
         then
             fileName=${BASH_REMATCH[2]}
-            path=${BASH_REMATCH[1]}/
+            path=${BASH_REMATCH[1]}
+            if [[ !(${path} =~ (.*)\/$) ]]
+            then
+                path="${path}/"
+            fi
 #            cp $file ${path}${fileName}-$(date +"%Y").crt
             certificates+=(${path}${fileName}.crt)
         elif [[ $file =~ ([^\/]*)\.pem$ ]]
@@ -50,32 +85,74 @@ renameCertificates() {
     done
 }
 
+# Check if file exist and if is not empty
+# $1 = file_to_check
+checkFile() {
+    file=$1
+    if [[ !(-f ${file}) || $(cat ${file}) = '' ]]
+    then
+        echo -e "${color_error}The file : ${file} does not exist or is empty"
+        exit
+    fi
+
+}
+
+
+# $1 = PATH/TO/file
+createdFileMessage() {
+    file=$1
+    if [[ -f ${file} ]]
+    then
+        echo -e "${color_success}File created with success ${color_info}${file}"
+    else
+        echo -e "${color_error}ERROR !${color_default}Cannot create file ${color_info}${file}"
+    fi
+}
+
 
 # $1 = path_to_certificates_directory
 # $2 = name_of_middle_certificate
 createFullChain() {
-    if [[ !(${path_to_certificates_directory} =~ (.*)\/$) ]]
-    then
-        path_to_certificates_directory="${path_to_certificates_directory}/"
-    fi
-    if [[ !(-d ${path_to_certificates_directory}${fileName}) ]]
-    then
-        mkdir ${path_to_certificates_directory}${fileName}
-    fi
+    addSlash
 
+    echo -e "Do you want use the apache/nginx conf template generator ?"
+    read -e -r -p "$ " result
+    if [[ ${result} =~ ^[yY]{1}e?s?.*$ ]]
+    then
+        useConfGenerator=true
+    else
+        useConfGenerator=false
+    fi
     for certificate in ${certificates[@]}
     do
-        echo $certificate
         if [[ $certificate =~ (.*)\/([^\/]*)\.crt$ ]];
         then
-
             fileName=${BASH_REMATCH[2]}
-            echo "${path_to_certificates_directory}${fileName}/${fileName}-$(date +"%Y")-fullchain.crt"
-            cat ${certificate} ${name_of_middle_certificate} > ${path_to_certificates_directory}${fileName}/${fileName}-$(date +"%Y")-fullchain.crt
+            echo -e "${color_info}$fileName beginning"
+            createVal 'company_name'
+            if [[ !(-d ${path_to_certificates_directory}${company_name}) ]]
+            then
+                mkdir ${path_to_certificates_directory}${company_name}
+            fi
+            if [[ $useConfGenerator = true ]]
+            then
+                createVal 'hostname'
+                ./conf_generator.sh ${company_name} ${hostname}
+            fi
+            checkFile ${certificate}
+            checkFile ${name_of_middle_certificate}
+            cat ${certificate} ${name_of_middle_certificate} > ${path_to_certificates_directory}${company_name}/${company_name}-$(date +"%Y")-fullchain.crt
+            createdFileMessage "${path_to_certificates_directory}${company_name}/${company_name}-$(date +"%Y")-fullchain.crt"
+            cat ${certificate} > ${path_to_certificates_directory}${company_name}/${company_name}-$(date +"%Y").crt
+            createdFileMessage "${path_to_certificates_directory}${company_name}/${company_name}-$(date +"%Y").crt"
+            cat ${certificate} ${name_of_middle_certificate} > ${path_to_certificates_directory}${company_name}/${company_name}-$(date +"%Y")-fullchain.pem
+            createdFileMessage "${path_to_certificates_directory}${company_name}/${company_name}-$(date +"%Y")-fullchain.pem"
         fi
     done
 
 }
+
+
 
 if [[ $1 ]]
 then
